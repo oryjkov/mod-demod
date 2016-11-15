40da2a7e1fdb63f0c85df1e839d587149bd1d572
olegr <oryjkov@gmail.com>
2016-11-15 21:17:52 +0100

    move send html-y stuff out of the js file

--- modulate.js
+++ modulate.js
@@ -1,12 +1,6 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var audioContext = new AudioContext();
-var textEncoder = new TextEncoder();
-var fcBox = null;
-var a_0Box = null;
-var a_1Box = null;
-var t_bBox = null;
-var inputMessageBox = null;
 
 var fs = 44100; // sampling frequency
 var fc = 10000; // carrier wave frequency
@@ -22,15 +16,7 @@ function byte_to_bits(b) {
   return retval;
 }
 
-function bits_to_byte(bits) {
-  var retval = 0;
-  for (var i = 0; i < 8; i += 1) {
-    retval = retval << 1 + bits[i];
-  }
-  return retval;
-}
-
-function string_to_ua(s) {
+function stringToUint8Array(s) {
   var char_buffer = new Uint8Array(s.length);
   for (var i = 0; i < s.length; i += 1) {
     char_buffer[i] = s.charCodeAt(i);
@@ -39,15 +25,8 @@ function string_to_ua(s) {
   return char_buffer;
 }
 
-function play() {
-  fc = parseInt(fcBox.value);
-  a_0 = parseFloat(a_0Box.value);
-  a_1 = parseFloat(a_1Box.value);
-  t_b = parseInt(t_bBox.value);
-  console.log(t_b);
-
-  var s = inputMessageBox.value;
-  var char_buffer = string_to_ua(s);
+function playMessage(messageString) {
+  var char_buffer = stringToUint8Array(messageString);
 
   var message = [];
   // This converts the char_buffer into an array of bits (each bit is 0 or 1).
@@ -67,19 +46,9 @@ function play() {
                  message[bit_index] == 1);
   }
   console.log(message);
-  //console.log("sending ", message.length, " bits");
 
   var bufferSource = audioContext.createBufferSource();
   bufferSource.connect(audioContext.destination);
   bufferSource.buffer = audioBuffer;
   bufferSource.start();
 }
-
-function onDOMLoad() {
-  fcBox = document.getElementById("fc");
-  a_0Box = document.getElementById("a_0");
-  a_1Box = document.getElementById("a_1");
-  t_bBox = document.getElementById("t_b");
-  inputMessageBox = document.getElementById("inputMessage");
-}
-document.addEventListener("DOMContentLoaded", onDOMLoad);
